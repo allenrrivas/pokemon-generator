2ebe17202bac8b8facbde505d325de0e85d67e44
allenrrivas <allenrrivas.30@gmail.com>
2023-09-12 19:18:59 -0700

    Random number between 1-151 diplays

---
+++ src/utils/getRandomPokemon.ts
@@ -0,0 +1,16 @@
+const MAX_DEX_ID = 151;
+
+export const getRandomPokemon: (notThisOne?: number) => number = (
+  notThisOne
+) => {
+  const pokedexNumber = Math.floor(Math.random() * MAX_DEX_ID) + 1;
+
+  if (pokedexNumber !== notThisOne) return pokedexNumber;
+  return getRandomPokemon(notThisOne);
+};
+
+export const getOptionsView = () => {
+    const firstId = getRandomPokemon();
+
+    return [firstId];
+}
