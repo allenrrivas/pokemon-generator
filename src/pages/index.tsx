5d1cc913a3070b58893a2d8aec680aa896a0d0ac
allenrrivas <allenrrivas.30@gmail.com>
2023-09-13 17:05:43 -0700

    Fixed error in build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const [ids, updateIds] = useState(() => getOptionsView());
   const [first] = ids;
 
-  const firstPokemon = api.pokemon.getById.useQuery({ id: first as number });
+  const firstPokemon = api.pokemon.getById.useQuery({ id: first! });
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
@@ -18,19 +18,19 @@ export default function Home() {
       <div className="p-2" />
       <div className="flex max-w-2xl items-center justify-between rounded border p-8">
         <div suppressHydrationWarning={true} className="h-44 w-44">
-          {/* {firstPokemon.data?.sprites.front_default && (
+          {firstPokemon.data?.sprites.front_default && (
             <Image
               src={firstPokemon.data?.sprites.front_default ?? ""}
               width={500}
               height={500}
               alt={firstPokemon.data?.name}
             />
-          )} */}
-          <img
+          )}
+          {/* <img
             src={firstPokemon.data?.sprites.front_default ?? ""}
             alt={firstPokemon.data?.name}
             className="w-full"
-          />
+          /> */}
           <div className="text-center text-xl capitalize">
             {firstPokemon.data?.name}
           </div>
