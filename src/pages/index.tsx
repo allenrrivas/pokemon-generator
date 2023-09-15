94ef3b87f0137592c8a51a3c591792ef7a13ee4f
allenrrivas <allenrrivas.30@gmail.com>
2023-09-14 19:08:52 -0700

    Fixed whitespace and phone view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,9 +14,9 @@ export default function Home() {
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <div className="text-center text-2xl">Random Pokemon Generator</div>
+      <div className="text-center text-2xl">Random Pokémon Generator</div>
       <div className="p-2" />
-      <div className="flex max-w-2xl items-center justify-between rounded border p-10">
+      <div className="flex max-w-2xl items-center justify-between rounded border px-3 py-10 lg:p-10">
         <div
           suppressHydrationWarning={true}
           className="grid h-48 w-96 grid-cols-2 gap-4"
@@ -36,12 +36,13 @@ export default function Home() {
           </div>
           <div>
             {firstPokemon.data?.stats.map((firstPokemon, index) => (
-              <div className="grid grid-cols-3 gap-4" key={index}>
-                <div className="capitalize">{firstPokemon.stat.name}</div>
-                <div>{firstPokemon.base_stat}</div>
+              <div className="grid grid-cols-2 gap-28" key={index}>
+                <div className="whitespace-nowrap capitalize">
+                  {firstPokemon.stat.name}: {firstPokemon.base_stat}
+                </div>
                 <div>
                   <progress
-                    className="progress progress-info w-20"
+                    className="progress progress-info w-8 lg:w-16"
                     value={firstPokemon.base_stat}
                     max="100"
                   ></progress>
